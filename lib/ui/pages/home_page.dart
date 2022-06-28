b4b1087c56b46798bc4a5db07cd9dc4315e38590
suhail shah <suhailshah372@gmail.com>
2022-06-28 17:54:22 +0700

    update homepage

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'dart:html';
-
+import 'package:airplane/ui/widgets/destination_card.dart';
+import 'package:airplane/ui/widgets/destination_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane/shared/theme.dart';
 
@@ -64,70 +64,92 @@ class HomePage extends StatelessWidget {
         margin: EdgeInsets.only(
           top: 30,
         ),
-        child: Row(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              DestinationCard(
+                name: 'Lake Ciliwung',
+                city: 'Tangerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                name: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
+              ),
+              DestinationCard(
+                name: 'Hill Heyo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                name: 'Menarra',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 5.0,
+              ),
+              DestinationCard(
+                name: 'Payung Teduh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.8,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget newDestinations() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+          left: defaultMargin,
+          right: defaultMargin,
+          bottom: 100,
+        ),
+        child: Column(
           children: [
-            Container(
-              width: 200,
-              height: 323,
-              margin: EdgeInsets.only(
-                left: defaultMargin,
+            Text(
+              'New This Year',
+              style: blackTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: semiBold,
               ),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                color: kWhiteColor,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    width: 180,
-                    height: 220,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/image_destination1.png',
-                        ),
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 55,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: kWhiteColor,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(18),
-                          ),
-                        ),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 24,
-                              height: 24,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    'assets/icon_star.png',
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              '4.8',
-                              style: blackTextStyle.copyWith(
-                                fontWeight: medium,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            ),
+            DestinationTile(
+              name: 'Danau Beratan',
+              city: 'Singajara',
+              imageUrl: 'assets/image_destination6.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Sydney Opera',
+              city: 'Australia',
+              imageUrl: 'assets/image_destination7.png',
+              rating: 4.7,
+            ),
+            DestinationTile(
+              name: 'Roma',
+              city: 'Italy',
+              imageUrl: 'assets/image_destination8.png',
+              rating: 4.8,
+            ),
+            DestinationTile(
+              name: 'Payung Teduh',
+              city: 'Singapore',
+              imageUrl: 'assets/image_destination9.png',
+              rating: 4.5,
+            ),
+            DestinationTile(
+              name: 'Hill Hey',
+              city: 'Monaco',
+              imageUrl: 'assets/image_destination10.png',
+              rating: 4.7,
             ),
           ],
         ),
@@ -138,6 +160,7 @@ class HomePage extends StatelessWidget {
       children: [
         header(),
         popularDestination(),
+        newDestinations(),
       ],
     );
   }
