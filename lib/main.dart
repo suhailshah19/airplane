a5fd082007b7f78caf1f649f0bf37c3070b7e158
suhail shah <suhailshah372@gmail.com>
2022-07-18 15:04:52 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+// }
+
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
